66f334d8c8779a660a4b5ef649b957471e56f4e7
krestininigor1963 <krestininigor1963@gmail.com>
2021-08-13 15:54:05 +0300

    finish middleware

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,8 @@
 
-import { createStore } from 'redux'
-import { rootReducer} from '../reducers'
+import {createStore, applyMiddleware} from 'redux'
+import {rootReducer} from '../reducers'
+import logger from 'redux-logger'
 
 
-export const store = createStore(rootReducer)
+export const store = createStore(rootReducer, applyMiddleware(logger)) // <-- добавляем
+//его в цепочку middleware'ов
